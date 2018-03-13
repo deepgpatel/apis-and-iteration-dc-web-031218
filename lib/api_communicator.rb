6f98c110893d4eac89a8740277d5329490772882
deepgpatel <deepgpatel93@gmail.com>
2018-03-13 17:01:49 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,22 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  character_name = character_hash["results"].find {|hash_result| hash_result["name"].downcase == character}
+  films_var = character_name["films"]
+  films_var.map do |film_url|
+    star_wars = RestClient.get(film_url)
+    star_hash = JSON.parse(star_wars)
+    star_hash['title']
+  end
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  puts films_hash
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,20 @@
 def welcome
-  # puts out a welcome message here!
+  puts "May The Force be with you"
+end
+
+def character_names
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+  character_hash["results"].map {|hash_result| hash_result["name"].downcase}
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a Character"
+  user_input = gets.chomp
+  if character_names.include?(user_input)
+    user_input
+  else
+  puts "The Force is not with them"
+  exit
+  end
 end
